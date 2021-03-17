b82daa3572cee8e4cf07bbf0284cdb39da07556c
LeeJeonDo <dlwjseh2@10x10.co.kr>
2021-03-17 13:57:34 +0900

    2021-03-17 Todo

---
+++ vue/search/Main/data.js
@@ -0,0 +1,52 @@
+const recent_keywords = [
+    { "keyword" : '다이어리' },
+    { "keyword" : '아이패드 파우치' },
+    { "keyword" : '스누피' },
+    { "keyword" : '에어팟' }
+];
+
+const best_keywords = {
+    "totalCount": 10,
+    "keywords": [
+        {
+            "keyword": "스누피",
+            "tag": "-"
+        },
+        {
+            "keyword": "다이어리케이스",
+            "tag": "-"
+        },
+        {
+            "keyword": "아이패드 파우치",
+            "tag": "-"
+        },
+        {
+            "keyword": "갤럭시z플립케이스",
+            "tag": "+1615"
+        },
+        {
+            "keyword": "마미버드",
+            "tag": "+165"
+        },
+        {
+            "keyword": "지퍼지갑",
+            "tag": "new"
+        },
+        {
+            "keyword": "파우치",
+            "tag": "+27"
+        },
+        {
+            "keyword": "15인치 노트북파우치",
+            "tag": "+11"
+        },
+        {
+            "keyword": "테이블보",
+            "tag": "+749"
+        },
+        {
+            "keyword": "펜트레이",
+            "tag": "-2"
+        }
+    ]
+}
